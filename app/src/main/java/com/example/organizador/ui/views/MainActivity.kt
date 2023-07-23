46840f1bccfc2cfddf5d2866670ddd1033b48b2b
Sharon Aylen Ostrovsky <saostrovsky@gylgroup.com>
2023-07-23 15:26:22 -0300

    se agrega autenticacion biometrica

--- app/src/main/java/com/example/organizador/ui/views/MainActivity.kt
+++ app/src/main/java/com/example/organizador/ui/views/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.organizador.ui.views
 
 import android.content.Intent
+import androidx.biometric.BiometricManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.organizador.R
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.biometric.BiometricPrompt.PromptInfo
+import androidx.core.content.ContextCompat
 import com.example.organizador.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: PromptInfo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,24 +29,95 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.expense -> {
-                    redirectGastos()
-                    true
-                }
-                R.id.todo -> {
-                    redirectToDo()
-                    true
-                }
-                R.id.graphPie ->{
-                    redirectGrafico()
-                    true
-                }
-                else -> false
+        switchModeListener()
+
+        binding.ivFingerPrint.setOnClickListener {
+            checkDeviceHasBiometric()
+        }
+
+        //createBiometricListener()
+
+        //createPromptInfo()
+
+
+
+    }
+
+    private fun createBiometricListener(){
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(this@MainActivity, errString, Toast.LENGTH_SHORT).show()
+                binding.tvInfo.text = errString
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                binding.tvInfo.text = "Authenticated failed"
+                Toast.makeText(this@MainActivity, "Authenticated failed", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                binding.tvInfo.text = "Authenticated success."
+                Toast.makeText(this@MainActivity, "Authenticated success", Toast.LENGTH_SHORT).show()
+                redirectGastos()
+            }
+        })
+    }
+
+    private fun createPromptInfo(){
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("CANCEL BIOMETRIC")
+            .build()
+    }
+
+    private fun checkDeviceHasBiometric(){
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)){
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                binding.tvInfo.text = "App can authenticate is using biometric."
+                createBiometricListener()
+                createPromptInfo()
+                biometricPrompt.authenticate(promptInfo)
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                binding.tvInfo.text = "No biometric feature available on this device."
+            }
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                binding.tvInfo.text = "Biometric feature are currently unavailable."
+            }
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                binding.tvInfo.text = "Device not enable biometric feature."
+            }
+            else -> {
+                binding.tvInfo.text = "Something went wrong!"
             }
         }
+    }
+
+    fun switchModeListener(){
+        val swDarkMode = binding.switchMode
+        swDarkMode.setOnCheckedChangeListener { _, isSelected ->
+            if (isSelected){
+                enableDarkMode()
+            }else{
+                disableDarkMode()
+            }
+        }
+    }
+
+    private fun enableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        delegate.applyDayNight()
+    }
+
+    private fun disableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        delegate.applyDayNight()
     }
 
     fun redirectGrafico(){
@@ -47,7 +129,7 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun redirectToDo(){
+    fun redirectToDoList(){
         val intent = Intent(this, ToDoListActivity::class.java)
         startActivity(intent)
     }
